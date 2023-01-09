056a888c6c1da471ad678a76ffe65840f4aa793e
paras9sid <sidharthjainp9@gmail.com>
2023-01-10 01:02:21 +0530

    Implemented Home/Firesore fetch post

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,45 @@
+import {useState,useEffect} from 'react';
+import { Link } from 'react-router-dom';
+import { firestore } from "../firebase";
+
+
 function Home() {
-  return <div className="">Home</div>;
+  const [posts,setPosts] = useState([]);
+
+  useEffect(()=>{
+    firestore
+    .collection('posts')
+    .get().then((snapshot)=>{
+      const posts = snapshot.docs.map((doc)=>{
+        return {
+          id:doc.id,
+          ...doc.data(),
+        };
+      });
+
+      console.log('posts',posts);
+      setPosts(posts); // will show/fect posts from cloud firestore and render on browser
+    });
+  },[]);
+
+  return <div className="home">
+    <h1>Tech Blog</h1>
+    <div id="blog-by">sidharth</div>
+
+    {/* //looping over posts */}
+    {posts.map((post,index)=>(
+
+      <div className='post' key = {`post-${index}`}>
+        <Link t0 ={`/post/${post.id}`}>
+          <h3>{post.title}</h3>
+        </Link>
+
+        <p>{post.subtitle}</p>
+      </div>  
+    ))}
+
+    
+  </div>;
 }
 
 export default Home;
--- src/firebase.js
+++ src/firebase.js
@@ -1,27 +1,12 @@
-
-// import firebase,{ initializeApp } from "firebase/app";
-// import 'firebase/firestore';
-
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 
-
-////firebase config fields and keys
-
+//firebase config fields and keys
 const firebaseConfig = {
 
-  apiKey: "AIzaSyBnHlUaj8_iZv598jYexzgsp8gr89PP1zE",
-  authDomain: "react-blog-904ef.firebaseapp.com",
-  projectId: "react-blog-904ef",
-  storageBucket: "react-blog-904ef.appspot.com",
-  messagingSenderId: "484345234843",
-  appId: "1:484345234843:web:2eed98d55c921b4f574d3e"
+ 
   
 };
 
-// const app = initializeApp(firebaseConfig);
-// export const firestore = firebase.firestore();
-
 firebase.initializeApp(firebaseConfig);
-
 export const firestore = firebase.firestore();
