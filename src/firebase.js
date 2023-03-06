55a0cfadfb101c700690167891fbe228401927f7
Sidharth Jain <104516201+paras9sid@users.noreply.github.com>
2023-03-06 15:13:50 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,7 @@ import "firebase/compat/firestore";
 const firebaseConfig = {
 
  
-    apiKey: "AIzaSyBnHlUaj8_iZv598jYexzgsp8gr89PP1zE",
-    authDomain: "react-blog-904ef.firebaseapp.com",
-    projectId: "react-blog-904ef",
-    storageBucket: "react-blog-904ef.appspot.com",
-    messagingSenderId: "484345234843",
-    appId: "1:484345234843:web:2eed98d55c921b4f574d3e"
+   //copy from project in firebase console settings.
   
 };
 
